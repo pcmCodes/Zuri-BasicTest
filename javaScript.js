41c5e2fc071bcedb99459a38a0e3695eaf58c8f7
Phemelo Mogane <101391794+pcmCodes@users.noreply.github.com>
2023-08-27 08:09:36 +0000

    Adding index.html and javaScript.js to file

---
+++ javaScript.js
@@ -0,0 +1,21 @@
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('require', 'displayfeatures');
+  ga('create', 'UA-25123446-47', 'auto');
+  ga('send', 'pageview');
+  
+var trackOutboundLink = function(url,type) {
+   ga('send', 'event', 'outbound', type, url, {'hitCallback':
+     function () {
+     	     //
+     }
+   });
+}
+  
+</script>
+
+
+
